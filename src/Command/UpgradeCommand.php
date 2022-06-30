f08e0be77178c9518a7bcd4e38f7c5f2e4c1b0e4
Deeka Wong <huangdijia@gmail.com>
2022-06-30 16:23:49 +0800

    Optimize

--- src/Command/UpgradeCommand.php
+++ src/Command/UpgradeCommand.php
@@ -50,7 +50,7 @@ class UpgradeCommand extends HyperfCommand
 
         $values = $confd->fetch();
 
-        $writer->setValues($values, true)->write();
+        $writer->setValues($values)->write();
 
         $this->logger->debug($template . ' is updated.');
     }
