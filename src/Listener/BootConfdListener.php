254c0e7493a2efe5122583cb3ab751b33e4ea2ca
Deeka Wong <huangdijia@gmail.com>
2022-06-30 15:16:51 +0800

    Optimized

--- src/Listener/BootConfdListener.php
+++ src/Listener/BootConfdListener.php
@@ -11,15 +11,21 @@ declare(strict_types=1);
 namespace FriendsOfHyperf\Confd\Listener;
 
 use FriendsOfHyperf\Confd\Confd;
+use Hyperf\Contract\ConfigInterface;
 use Hyperf\Coordinator\Constants;
 use Hyperf\Coordinator\CoordinatorManager;
 use Hyperf\Event\Annotation\Listener;
 use Hyperf\Event\Contract\ListenerInterface;
 use Hyperf\Framework\Event\MainWorkerStart;
+use Psr\Container\ContainerInterface;
 
 #[Listener()]
 class BootConfdListener implements ListenerInterface
 {
+    public function __construct(private ContainerInterface $container, private ConfigInterface $config)
+    {
+    }
+
     public function listen(): array
     {
         return [
@@ -29,8 +35,11 @@ class BootConfdListener implements ListenerInterface
 
     public function process(object $event): void
     {
-        $confd = $this->container->get(Confd::class);
-        $confd->watch();
+        if (! $this->config->has('confd.default')) {
+            return;
+        }
+
+        $this->container->get(Confd::class)->watch();
 
         while (true) {
             $isExited = CoordinatorManager::until(Constants::WORKER_EXIT)->yield(1);
