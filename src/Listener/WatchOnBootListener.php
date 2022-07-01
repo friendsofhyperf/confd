1a7993346ae74da15d8af01343e8a4fa7030e75c
Deeka Wong <huangdijia@gmail.com>
2022-07-01 08:07:26 +0800

    Optimize output

--- src/Listener/WatchOnBootListener.php
+++ src/Listener/WatchOnBootListener.php
@@ -37,7 +37,7 @@ class WatchOnBootListener implements ListenerInterface
     public function process(object $event): void
     {
         $this->confd->watch();
-        $this->logger->debug('Confd watch started.');
+        $this->logger->debug('Confd start watching.');
 
         while (true) {
             $isWorkerExited = CoordinatorManager::until(Constants::WORKER_EXIT)->yield(1);
